2c0a8bbcc3e339b451f7cf2a18f70719b19f182d
Pin Gu <i@pingu.moe>
2023-11-16 10:55:51 +0800

    compose service dependencies

--- shared-init.sh
+++ shared-init.sh
@@ -1,4 +1,6 @@
 # !/bin/bash
 
-docker network create dev-infra
-docker compose -f shared.docker-compose.yml up -d
+[ ! "$(docker network ls | grep dev-infra)" ] \
+  && docker network create dev-infra \
+  || echo Docker network \"dev-infra\" is exist.
+docker compose --file shared.docker-compose.yml --profile debug up -d
